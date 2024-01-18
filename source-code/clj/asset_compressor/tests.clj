dba6698ce5caa879d5bedbbb3d3e8b4b1fe79f5c
Bit Handshake <adam@monotech.hu>
2024-01-18 23:54:50 +0100

    0.0.8.3

--- source-code/clj/asset_compressor/tests.clj
+++ source-code/clj/asset_compressor/tests.clj
@@ -13,7 +13,7 @@
 ;
 ; @constant (map)
 (def OPTIONS-TEST
-     {:compressor-f     {:f* fn?                                      :e* ":compressor-f must be a function!"}
-      :filename-pattern {:f* regex/pattern?                           :e* ":filename-pattern must be a regex pattern!"}
-      :output-path      {:f* string? :not* empty?                     :e* ":output-path must be a nonempty string!"}
-      :source-paths     {:and* [vector? #(every? string/nonempty? %)] :e* ":source-paths must be a vector with nonempty string items!"}})
+     {:compressor-f     {:f* fn?                                       :e* ":compressor-f must be a function!"}
+      :filename-pattern {:f* regex/pattern?                            :e* ":filename-pattern must be a regex pattern!"}
+      :output-path      {:f* string? :not* empty?                      :e* ":output-path must be a nonempty string!"}
+      :source-paths     {:and* [vector? #(every? string/not-empty? %)] :e* ":source-paths must be a vector with nonempty string items!"}})
